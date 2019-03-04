2d76c571dfccd2fc4dc1053538e68b32935bdeb3
André Wagner <andre.nho@gmail.com>
2019-03-04 09:46:06 -0300

    .

--- coffee/src/Home.js
+++ coffee/src/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home(props) {
-    return <div />;
+    return (
+        <div>
+            <div className="col-md-5 p-lg-5 mx-auto my-5 text-center">
+                <h1 className="display-4 font-weight-bold">The best cup of coffee, anywhere.</h1>
+                <p className="lead font-weight-normal">
+                    Customize your cup of coffee and have it delievered to your home or office, fast!
+                </p>
+                <Link className="btn btn-lg btn-primary" to="/order">Place your order</Link>
+              </div>
+        </div>
+    );
 }
 
 // vim:st=4:sts=4:sw=4:expandtab
