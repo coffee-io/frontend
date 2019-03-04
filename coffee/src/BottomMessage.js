f6ca2c4a01e1977dc39b996e75ea14c5b2903ad0
André Wagner <andre.nho@gmail.com>
2019-03-04 10:06:20 -0300

    .

--- coffee/src/BottomMessage.js
+++ coffee/src/BottomMessage.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function BottomMessage(props) {
-    return (
-        <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-danger">
-            <span className="navbar-text">
-                IMPORTANT! This is not a real website, it serves as my portfolio for web development on cloud.
-                See more about the <Link to="/architecture">serverless cloud architecture</Link> used on this website.
-            </span>
-        </nav>
-    );
+export default class BottomMessage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { visible: true };
+    }
+
+    dismiss = () => {
+        this.setState({ visible: false });
+    }
+
+    render () {
+        const cl = "navbar fixed-bottom navbar-expand-sm navbar-dark bg-danger " + (!this.state.visible ? "d-none" : "");
+        return (
+            <nav className={cl}>
+                <span className="navbar-text">
+                    IMPORTANT! This is not a real website, it serves as my portfolio for web development on cloud.
+                    See more about the <Link to="/architecture">serverless cloud architecture</Link> used on this website.
+                    (<a href="#!" onClick={this.dismiss}>Dismiss</a>)
+                </span>
+            </nav>
+        );
+    }
 }
 
 // vim:st=4:sts=4:sw=4:expandtab
