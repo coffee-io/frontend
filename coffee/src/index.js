c50dd8b70aa1408a50c35631f88aedbdf0f4d552
Andre Wagner <andre.nho@gmail.com>
2019-03-07 20:26:51 -0300

    .

--- coffee/src/index.js
+++ coffee/src/index.js
@@ -1,15 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './configureStore';
 import './custom.scss';
+
+const store = configureStore();
 
 window.jQuery = window.$ = require('jquery/dist/jquery.min.js');
 require('bootstrap/dist/js/bootstrap.min.js');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
