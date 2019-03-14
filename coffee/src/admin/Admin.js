e5ad0e65bf62fa949561438e94800fa50223a95c
Andre Wagner <andre.nho@gmail.com>
2019-03-14 20:19:06 +0000

    .

--- coffee/src/admin/Admin.js
+++ coffee/src/admin/Admin.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-export default function Admin(props) {
-    return <p>Not implemented</p>;
+const imageStyle = {
+    marginTop: "20px",
 }
+
+class Admin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+            items: [],
+        };
+    }
+
+    componentDidMount() {
+        axios.get(process.env.REACT_APP_BACKEND_URL + '/orders')
+            .then(res => this.setState({ loaded: true, items: res, }))
+            .catch(error => { throw(error); });
+    }
+
+    renderData() {
+        if (!this.state.loaded)
+            return <FontAwesomeIcon icon={faSpinner} style={imageStyle} size="4x" className="d-inline-block align-middle" pulse />;
+        return (
+            <div>
+                Not implemented.
+            </div>
+        );
+        // https://getbootstrap.com/docs/4.0/components/collapse/
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="py-5 text-center">
+                    <p className="lead">List of requests ordered today.</p>
+                    {this.renderData()}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Admin;
 
 // vim:st=4:sts=4:sw=4:expandtab
