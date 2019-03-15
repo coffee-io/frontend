03daff06b2c5de040f3757a3860a3e8a4c18e6f9
Andre Wagner <andre.nho@gmail.com>
2019-03-15 04:14:29 +0000

    .

--- coffee/src/architecture/Architecture.js
+++ coffee/src/architecture/Architecture.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import architecture from './architecture.png';
 
 export default function Architecture(props) {
-    return <p>Not implemented</p>;
+    return (
+        <div className="container-fluid">
+            <main className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content">
+                <h3>Application architecture</h3>
+                <a href={architecture}>
+                    <img src={architecture} alt="Cloud architecture" className="img-fluid" />
+                </a>
+                <p><i>Click on the image to increase the size.</i></p>
+            </main>
+        </div>
+    );
 }
 
 // vim:st=4:sts=4:sw=4:expandtab
